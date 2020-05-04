47aa16ce2ec6916905bfffbe8d0e60835d4ba35e
dingchao <1109928059@qq.com>
2020-05-04 16:04:14 +0800

    拥有最多糖果的孩子

---
+++ src/com/Test27_kidsWithCandies.java
@@ -0,0 +1,59 @@
+package com;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 1431.拥有糖果最多的孩子
+ *
+ * 给你一个数组 candies 和一个整数 extraCandies ，
+ * 其中 candies[i] 代表第 i 个孩子拥有的糖果数目。
+ *
+ * 对每一个孩子，检查是否存在一种方案，
+ * 将额外的 extraCandies 个糖果分配给孩子们之后，此孩子有 最多 的糖果。
+ *
+ */
+public class Test27_kidsWithCandies {
+    public static void main(String[] args) {
+
+    }
+
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> list = new ArrayList<>(candies.length);
+
+        int max = 0;
+        for(int i=0;i<candies.length;i++){
+            if(candies[i] >= max){
+                max = candies[i];
+            }
+        }
+
+        for(int i=0;i<candies.length;i++){
+            if(candies[i]+extraCandies >= max){
+                list.add(true);
+            }else {
+                list.add(false);
+            }
+        }
+
+        return list;
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
