f54a555a710d3719bcfbef50eaa17f7f16e583b0
boluo1997 <1109928059@qq.com>
2021-05-04 23:18:55 +0800

    统计位数为偶数的数字

---
+++ src/com/LeetCode1295_findNumbers.java
@@ -0,0 +1,19 @@
+package com;
+
+/**
+ * 1295. 统计位数为偶数的数字
+ * 给你一个整数数组 nums，请你返回其中位数为 偶数 的数字的个数。
+ */
+public class LeetCode1295_findNumbers {
+    public int findNumbers(int[] nums) {
+
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            String str = String.valueOf(nums[i]);
+            if (str.length() % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
