6c76bca1292bbd21f1195bd4ac47e97516c4c22f
boluo1997 <1109928059@qq.com>
2021-05-04 23:12:53 +0800

    按既定顺序创建目标数组

--- src/com/LeetCode1389_createTargetArray.java
+++ src/com/LeetCode1389_createTargetArray.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
  * 按从左到右的顺序依次读取 nums[i] 和 index[i]，在 target 数组中的下标 index[i] 处插入值 nums[i] 。
  * 重复上一步，直到在 nums 和 index 中都没有要读取的元素。
  * 请你返回目标数组。
- * 
+ *
  * 题目保证数字插入位置总是存在。
  */
 public class LeetCode1389_createTargetArray {
