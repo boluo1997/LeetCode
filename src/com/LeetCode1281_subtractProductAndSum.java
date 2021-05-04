3d2c3047477c6d03589e62aa51134967e40e6dd4
boluo1997 <1109928059@qq.com>
2021-05-04 23:03:28 +0800

    整数的各位积和之差

---
+++ src/com/LeetCode1281_subtractProductAndSum.java
@@ -0,0 +1,23 @@
+package com;
+
+/**
+ * 整数的各位积和之差
+ * 给你一个整数 n，请你帮忙计算并返回该整数「各位数字之积」与「各位数字之和」的差。
+ */
+public class LeetCode1281_subtractProductAndSum {
+
+    public int subtractProductAndSum(int n) {
+        int sum = 0;
+        int product = 1;
+
+        while (n > 0) {
+
+            int num = n % 10;
+            product *= num;
+            sum += num;
+            n = n / 10;
+        }
+
+        return product - sum;
+    }
+}
